2803139fe17a698eb2e1693ad1e3b6a7ca296639
Angie Mojica <nataliamojica90@gmail.com>
2024-04-04 20:05:19 -0500

    Update URL Servers

--- src/main/java/edu/escuelaing/arep/HttpConection.java
+++ src/main/java/edu/escuelaing/arep/HttpConection.java
@@ -10,10 +10,11 @@ public class HttpConection {
 
     private static final String USER_AGENT = "Mozilla/5.0";
     private static int serverIndex = 0;
-    private static final String[] SERVERS = new String[] { "ec2-54-91-215-220.compute-1.amazonaws.com:4568/", "ec2-54-91-215-220.compute-1.amazonaws.com:4568/"};
+    private static final String[] SERVERS = new String[] { "http://54.242.20.242:4568/", "http://52.73.39.133:4568/"};
 
     public static String invoke(String operation, String list, String value) throws IOException {
         String GET_URL = SERVERS[serverIndex] + operation + "?numbers=" + list + "&number=" + value;
+        changeServer();
         System.out.println("GET URL: " + GET_URL);
         URL obj = new URL(GET_URL);
         HttpURLConnection con = (HttpURLConnection) obj.openConnection();
@@ -34,7 +35,6 @@ public class HttpConection {
             System.out.println("GET request not worked");
         }
         System.out.println("GET DONE");
-        changeServer();
         System.out.println("Server changed to: " + serverIndex);
         return response.toString();
     }
